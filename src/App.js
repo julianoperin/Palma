c0c43dac7126e12bd62505865d15e6936d17c7c3
‘Juliano <julianoperins@gmail.com>
2021-02-09 16:00:58 -0500

    Started working on the Navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import "./App.css";
+// import Navbar from "./components";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Test</h1>
+    <div>
+      <h1>Started...</h1>
+      {/* <Navbar /> */}
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.elements.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavbarElements = () => {
+  return (
+    <div>
+      <h1>navbar Elements</h1>
+    </div>
+  );
+};
+
+export default NavbarElements;
