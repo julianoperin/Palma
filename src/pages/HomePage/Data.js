bc227e4150034ebc765c8667f92a5ca791374558
‘Juliano <julianoperins@gmail.com>
2021-02-09 18:58:42 -0500

    Working on styles

---
+++ src/pages/HomePage/Data.js
@@ -0,0 +1,69 @@
+export const homeObjOne = {
+  primary: false,
+  lightBg: true,
+  lightTopLine: false,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "New York City",
+  headline:
+    "Over 100 years of combined experience and 3 Billion recovered for our clients",
+  description:
+    "We fight for life-changing results that provide families with hope for a better future. ",
+  buttonLabel: "Get a Free Case Review",
+  imgStart: "",
+  img: require("../../images/lawyers.jpg"),
+  alt: "Credit Card",
+  start: "",
+};
+
+export const homeObjTwo = {
+  primary: true,
+  lightBg: false,
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Instant Setup",
+  headline: "Extremely quick onboarding process",
+  description:
+    "Once you've joined, our team of specialist will reach out to you and get you set up in minutes.",
+  buttonLabel: "Get a Free Case Review",
+  imgStart: "",
+  img: require("../../images/svg-2.svg"),
+  alt: "Vault",
+  start: "",
+};
+
+export const homeObjThree = {
+  primary: false,
+  lightBg: true,
+  lightTopLine: false,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Sarah Jeni",
+  headline:
+    "Ultra helped me increase my revenue by over 3X in less than 3 months!",
+  description:
+    "Their team is wonderful! I can't believe I didn't start working with them earlier.",
+  buttonLabel: "View Case Study",
+  imgStart: "start",
+  img: require("../../images/profile.jpg"),
+  alt: "Vault",
+  start: "true",
+};
+
+export const homeObjFour = {
+  primary: true,
+  lightBg: false,
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Secure Database",
+  headline: "All your data is stored on our secure server",
+  description:
+    "You will never have to worry about your information getting leaked. Our team of security experts will ensure your records are kept safe.",
+  buttonLabel: "Sign Up Now",
+  imgStart: "start",
+  img: require("../../images/svg-3.svg"),
+  alt: "Vault",
+  start: "true",
+};
